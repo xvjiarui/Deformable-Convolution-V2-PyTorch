a9d71c2fdbc49758bbfa5865195060f112a121ff
Jiarui XU <xvjiarui0826@gmail.com>
2019-01-17 02:00:16 +0800

    add deform conv 3d cpp and cuda src

--- src/vision.cpp
+++ src/vision.cpp
@@ -2,6 +2,7 @@
 #include "deform_psroi_pooling.h"
 #include "deform_conv.h"
 #include "modulated_deform_conv.h"
+#include "deform_conv3d.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("deform_conv_forward", &deform_conv_forward, "deform_conv_forward");
@@ -10,4 +11,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("modulated_deform_conv_backward", &modulated_deform_conv_backward, "modulated_deform_conv_backward");
   m.def("deform_psroi_pooling_forward", &deform_psroi_pooling_forward, "deform_psroi_pooling_forward");
   m.def("deform_psroi_pooling_backward", &deform_psroi_pooling_backward, "deform_psroi_pooling_backward");
+  m.def("deform_conv3d_forward", &deform_conv3d_forward, "deform_conv3d_forward");
+  m.def("deform_conv3d_backward", &deform_conv3d_backward, "deform_conv3d_backward");
 }
