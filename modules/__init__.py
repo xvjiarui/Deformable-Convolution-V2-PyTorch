f5a4e0af95e76f6a333d2c1a0dcb69dfa532bf0e
Jiarui XU <xvjiarui0826@gmail.com>
2019-01-26 18:33:23 +0800

    add sparse 2d conv

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,5 @@
 from .deform_conv2d import DeformConv2d, _DeformConv2d, DeformConv2dPack
+from .sparse_conv2d import SparseConv2d, _SparseConv2d, SparseConv2dPack
 from .modulated_deform_conv2d import ModulatedDeformConv2d, _ModulatedDeformConv2d, ModulatedDeformConv2dPack
 from .deform_psroi_pooling import DeformRoIPooling, _DeformRoIPooling, DeformRoIPoolingPack
 
